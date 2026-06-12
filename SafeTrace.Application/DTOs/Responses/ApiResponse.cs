3a293679ab5201e375749fdd73d2d58f9bb3f20f
Gargera <et93512@gmail.com>
2026-06-12 07:10:24 +0300

    Add StatusCode to ApiResponse

--- SafeTrace.Application/DTOs/Responses/ApiResponse.cs
+++ SafeTrace.Application/DTOs/Responses/ApiResponse.cs
@@ -4,13 +4,13 @@
     {
         public bool Success { get; set; }
         public string Message { get; set; } = null!;
+        public int StatusCode { get; set; }
         public T? Data { get; set; }
 
-        public static ApiResponse<T> Ok(T? data = default, string message = "Success")
-            => new() { Success = true, Message = message, Data = data };
+        public static ApiResponse<T> Ok(T? data = default, string message = "Success", int statusCode = 200)
+            => new() { Success = true, Message = message, Data = data, StatusCode = statusCode };
 
-        public static ApiResponse<T> Fail(string message = "Failed")
-            => new() { Success = false, Message = message };
-
+        public static ApiResponse<T> Fail(string message = "Failed", int statusCode = 400)
+            => new() { Success = false, Message = message, StatusCode = statusCode };
     }
 }
