19760d7753c764ab8e3ed788640ebb253b183182
Gargera <et93512@gmail.com>
2026-06-16 17:42:07 +0300

    Remove StatusCode from ApiResponse<T>

--- SafeTrace.Application/DTOs/Responses/ApiResponse.cs
+++ SafeTrace.Application/DTOs/Responses/ApiResponse.cs
@@ -4,13 +4,12 @@
     {
         public bool Success { get; set; }
         public string Message { get; set; } = null!;
-        public int StatusCode { get; set; }
         public T? Data { get; set; }
 
-        public static ApiResponse<T> Ok(T? data = default, string message = "Success", int statusCode = 200)
-            => new() { Success = true, Message = message, Data = data, StatusCode = statusCode };
+        public static ApiResponse<T> Ok(T? data = default, string message = "Success")
+            => new() { Success = true, Message = message, Data = data};
 
-        public static ApiResponse<T> Fail(string message = "Failed", int statusCode = 400)
-            => new() { Success = false, Message = message, StatusCode = statusCode };
+        public static ApiResponse<T> Fail(string message = "Failed")
+            => new() { Success = false, Message = message};
     }
 }
