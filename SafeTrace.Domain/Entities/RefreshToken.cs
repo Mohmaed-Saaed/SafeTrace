6a14caa6b7b3ba2762e4a0ad96a100d27f9c83b2
Mohamed Saeed <muhameds913@gmail.com>
2026-06-17 19:35:06 +0300

    Update

---
+++ SafeTrace.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace SafeTrace.Domain.Entities
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Token { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+        public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? RevokedAt { get; set; }
+        public string? ReplacedByToken { get; set; }
+        public bool IsActive => RevokedAt == null && !IsExpired;
+
+        [Required]
+        public string UserId { get; set; } = null!;
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
---
+++ SafeTrace.Domain/Entities/UserOTP.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafeTrace.Domain.Entities
+{
+    public class UserOtp
+    {
+        public int Id { get; set; }
+        public string Code { get; set; } = null!;
+        public OtpType Type { get; set; }
+        public DateTime ExpiryTime { get; set; }
+        public bool IsUsed { get; set; }
+
+        public string UserId { get; set; } = null!;
+        public ApplicationUser User { get; set; } = null!;
+    }
+}
