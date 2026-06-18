a91aa955e6f5b3eaea297820b6e3d734930ed0fc
Yousef721 <yousarsefabedalla255@gmail.com>
2026-06-18 14:23:28 +0300

    Update

--- SafeTrace.Domain/Entities/UrgentCase.cs
+++ SafeTrace.Domain/Entities/UrgentCase.cs
@@ -5,7 +5,6 @@ namespace SafeTrace.Domain.Entities
     public class UrgentCase : BaseCase
     {
         public Point Location { get; set; } = default!;
-        public Point Location { get; set; } = default!;
         public DateTime EndDate { get; set; }
         public DateTime LimitReachDate { get; set; }
     }
