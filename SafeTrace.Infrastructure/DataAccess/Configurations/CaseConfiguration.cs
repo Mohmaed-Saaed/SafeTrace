53bc20c1c4c32d91ccfe7e2e52442d01530f97c8
Gargera <et93512@gmail.com>
2026-06-19 23:06:39 +0300

    fix disciminator

--- SafeTrace.Infrastructure/DataAccess/Configurations/CaseConfiguration.cs
+++ SafeTrace.Infrastructure/DataAccess/Configurations/CaseConfiguration.cs
@@ -65,10 +65,9 @@ namespace SafeTrace.Infrastructure.DataAccess.Configurations
 
 
             builder.HasDiscriminator<string>("Discriminator")
-            .HasValue<Case>("Case")
-            .HasValue<UrgentCase>("UrgentCase")
-            .HasValue<LongTermMissingCase>("LongTermMissingCase")
-            .HasValue<UnknownCase>("UnknownCase");
+                .HasValue<UrgentCase>("UrgentCase")
+                .HasValue<LongTermMissingCase>("LongTermMissingCase")
+                .HasValue<UnknownCase>("UnknownCase");
         }
     }
 }
