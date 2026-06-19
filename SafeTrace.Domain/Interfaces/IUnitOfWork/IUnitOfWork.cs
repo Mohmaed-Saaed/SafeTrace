479706f2ed9d45eaea50fc35328c1bc98c9de0b4
Gargera <et93512@gmail.com>
2026-06-19 23:55:46 +0300

    fix unitOfWork to handle TPH

--- SafeTrace.Domain/Interfaces/IUnitOfWork/IUnitOfWork.cs
+++ SafeTrace.Domain/Interfaces/IUnitOfWork/IUnitOfWork.cs
@@ -2,19 +2,7 @@
 {
     public interface IUnitOfWork : IDisposable
     {
-        public IRepository<RefreshToken> RefreshTokenRepository { get;}
-        public IRepository<UserOtp> UserOtpRepository { get;}
-        //public IRepository<UrgentCase> UrgentCaseRepository { get;}
-        //public IRepository<LongTermMissingCase> LongTermCaseRepository { get;}
-        //public IRepository<UnknownCase> UnknownCaseRepository { get;}
-        public IRepository<Case> CaseRepository { get;}
-        public IRepository<CasePhoto> CasePhotoRepository { get;}
-        public IRepository<Chat> ChatRepository { get;}
-        public IRepository<Complaint> ComplaintRepository { get;}
-        public IRepository<FoundPersonInfo> FoundPersonInfoRepository { get;}
-        public IRepository<Message> MessageRepository { get;}
-        public IRepository<Notification> NotificationRepository { get;}
-        public IRepository<AgeCategory> AgeCategoryRepository { get;}
+        IRepository<TEntity> Repository<TEntity>() where TEntity : class;
 
         Task<int> SaveAsync();
 
