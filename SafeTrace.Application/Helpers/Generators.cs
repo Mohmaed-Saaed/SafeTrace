5e67327f2d6ec8bf1c2208cef950dab02c79a7bd
Gargera <et93512@gmail.com>
2026-06-19 16:52:42 +0300

    Add Generators helper for case code generation

---
+++ SafeTrace.Application/Helpers/Generators.cs
@@ -0,0 +1,10 @@
+﻿namespace SafeTrace.Application.Helpers
+{
+    public static class Generators
+    {
+        public static string GenerateCaseCode()
+        {
+            return $"LT-{DateTime.UtcNow:yyyyMMdd}-{Guid.NewGuid().ToString("N")[..6].ToUpperInvariant()}";
+        }
+    }
+}
