bbbbac7ef7ad84f06b57d4a43954c5e876861469
Gargera <et93512@gmail.com>
2026-06-21 00:19:14 +0300

    add OTP service

--- SafeTrace.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
+++ SafeTrace.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
@@ -30,6 +30,7 @@ namespace SafeTrace.Infrastructure.DependencyInjection
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IAccountService, AccountService>();
             services.AddScoped<IEmailService, EmailService>();
+            services.AddScoped<IOtpService, OtpService>();
             services.AddScoped<IRolePermissionService, RolePermissionService>();
             services.AddScoped<IUserService, UserService>();
 
