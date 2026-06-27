952429434c43bf338d36afc185e309070042ea50
Yousef721 <yousarsefabedalla255@gmail.com>
2026-06-27 23:23:00 +0300

    Update

--- SafeTrace/Program.cs
+++ SafeTrace/Program.cs
@@ -55,6 +55,7 @@ namespace SafeTrace
 
             builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
             builder.Services.AddProblemDetails();
+            builder.Services.AddSignalR();
 
             var app = builder.Build();
 
