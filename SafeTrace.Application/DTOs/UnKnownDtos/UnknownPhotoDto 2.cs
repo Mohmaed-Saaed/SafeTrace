b62630a5793f391ffec1993b2866af07542d9519
Yousef721 <yousarsefabedalla255@gmail.com>
2026-06-30 03:00:16 +0300

    Merge Version 1

---
+++ SafeTrace.Application/DTOs/UnKnownDtos/UnknownPhotoDto 2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafeTrace.Application.DTOs.UnKnownDtos
+{
+    public class UnknownPhotoDto
+    {
+        public int PhotoId { get; set; }
+        public string ImagePath { get; set; } = null!;
+        public bool IsPrimary { get; set; }
+    }
+}
