da8cd273ec74f6d14c44eae7a6a3e602ad77b368
Yousef721 <yousarsefabedalla255@gmail.com>
2026-07-16 14:15:20 +0300

    Remove MyCases From Cases And Add In User Profile

--- SafeTrace.Application/Interfaces/IServices/IUserProfile/IUserProfileService.cs
+++ SafeTrace.Application/Interfaces/IServices/IUserProfile/IUserProfileService.cs
@@ -15,8 +15,6 @@ namespace SafeTrace.Application.Interfaces.IServices.IUserProfile
         Task<ApiResponse<bool>> UpdateHomeLocationAsync(string userId, UpdateHomeLocationDTO dto);
         Task<ApiResponse<bool>> UpdatePhoneNumberAsync(string userId, ChangePhoneNumberDTO dto);
         Task<ApiResponse<bool>> AddIdImageAsync(string userId, AddIdImageDTO dto);
-
+        Task<ApiResponse<PaginationResponseDto<MyCaseListItemDto>>> GetMyCasesAsync(string userId, MyCasesFilterDto filter);
     }
-
-
 }
