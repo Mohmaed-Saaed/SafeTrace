afc56ecd38288ef39a26ff5ef3caf6cb0ded0826
Gargera <et93512@gmail.com>
2026-07-24 06:39:37 +0300

    minimize RoleName length

--- SafeTrace.Application/DTOs/RolePermission/Request/CreateRoleDto.cs
+++ SafeTrace.Application/DTOs/RolePermission/Request/CreateRoleDto.cs
@@ -5,7 +5,7 @@ namespace SafeTrace.Application.DTOs.RolePermission.Request
     public class CreateRoleDto
     {
         [Required(ErrorMessage = "اسم الدور مطلوب.")]
-        [MaxLength(100, ErrorMessage = "اسم الدور لا يمكن أن يتجاوز 100 حرف.")]
+        [MaxLength(50, ErrorMessage = "اسم الدور لا يمكن أن يتجاوز 50حرف.")]
         public string RoleName { get; set; } = null!;
     }
 }
