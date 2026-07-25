d2b1fcdf18d45e0d0a7d9d3c609f18524480722b
Yousef721 <yousarsefabedalla255@gmail.com>
2026-07-25 19:05:21 +0300

    feat: add UrgentEventDateAttribute and relocate EventDate property from base DTO to individual case DTOs for specialized validation.

--- SafeTrace.Application/DTOs/LongTermCase/Request/CreateLongTermCaseDto.cs
+++ SafeTrace.Application/DTOs/LongTermCase/Request/CreateLongTermCaseDto.cs
@@ -1,10 +1,17 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
+using SafeTrace.Application.Common.Validators.Attributes;
 using SafeTrace.Application.DTOs.Cases.Request;
 
 namespace SafeTrace.Application.DTOs.LongTermCase.Request
 {
     public class CreateLongTermCaseDto : CaseCreateBaseDto
     {
+        [Required(ErrorMessage = "Event date is required.")]
+        [PastDate(ErrorMessage = "Event date cannot be in the future.")]
+        [DataType(DataType.Date)]
+        public DateTime? EventDate { get; set; }
+
         [Required(ErrorMessage = "First name is required.")]
         [ArabicText(ErrorMessage = "First name must contain Arabic letters and spaces only.")]
         [StringLength(60, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 60 characters.")]
--- SafeTrace.Application/DTOs/UnKnownCase/Request/CreateUnknownDto.cs
+++ SafeTrace.Application/DTOs/UnKnownCase/Request/CreateUnknownDto.cs
@@ -1,9 +1,16 @@
-﻿using SafeTrace.Application.DTOs.Cases.Request;
+using System.ComponentModel.DataAnnotations;
+using SafeTrace.Application.Common.Validators.Attributes;
+using SafeTrace.Application.DTOs.Cases.Request;
 
 namespace SafeTrace.Application.DTOs.UnKnownCase.Request
 {
     public class CreateUnknownDto : CaseCreateBaseDto
     {
+        [Required(ErrorMessage = "Event date is required.")]
+        [PastDate(ErrorMessage = "Event date cannot be in the future.")]
+        [DataType(DataType.Date)]
+        public DateTime? EventDate { get; set; }
+
         [ArabicText(ErrorMessage = "First name must contain Arabic letters and spaces only.")]
         [StringLength(60, MinimumLength = 2, ErrorMessage = "First name must be between 2 and 60 characters.")]
         public string? FName { get; set; }
