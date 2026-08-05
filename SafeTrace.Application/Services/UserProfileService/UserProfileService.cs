595e817125392a9fc3529a20c4c673887da440e0
Youssef farouk <faroukyousef0@gmail.com>
2026-08-05 13:35:52 +0300

    no thing

--- SafeTrace.Application/Services/UserProfileService/UserProfileService.cs
+++ SafeTrace.Application/Services/UserProfileService/UserProfileService.cs
@@ -124,7 +124,7 @@ namespace SafeTrace.Application.Services.UserProfileServices
                 {
                     throw new BadRequestException("صورة البطاقة موجودة بالفعل .");
                 }
-                
+
                 var newIdImageFront =
                  await _Image.SaveFileAsync(dto.IdentificationImageFront, "Identification/Front");
                 var newIdImageBack =
@@ -166,16 +166,9 @@ namespace SafeTrace.Application.Services.UserProfileServices
         public async Task<ApiResponse<bool>> UpdateNameAsync(string userId, UpdateNameDTO dto)
         {
             var user = await GetUser(userId);
-            //if (user == null)
-            //{
-            //    throw new NotFoundException("المستخدم غير موجود");
-            //}
-            //else
-            //{
             _mapper.Map(dto, user);
             var result = await _userManager.UpdateAsync(user);
             return ApiResponse<bool>.Ok(true, "تم تحديث الاسم بنجاح");
-            //}
         }
 
 
@@ -196,7 +189,6 @@ namespace SafeTrace.Application.Services.UserProfileServices
                 if (!result.Succeeded)
                 {
                     string.Join(", ", result.Errors.Select(e => e.Description));
-                    //throw new BadRequestException("حدث خطأ اثناء محاولة اضافة صورة");
                     return ApiResponse<bool>.Ok(false, "حدث خطأ اثناء محاولة اضافة صورة");
                 }
             }
@@ -207,10 +199,6 @@ namespace SafeTrace.Application.Services.UserProfileServices
         public async Task<ApiResponse<bool>> RemoveProfileImageAsync(string userId)
         {
             var user = await GetUser(userId);
-
-            //if (user is null)
-            //    throw new NotFoundException("المستخدم غير موجود.");
-
             if (string.IsNullOrWhiteSpace(user.ProfileImage))
                 return ApiResponse<bool>.Ok(true, "لا توجد صورة شخصية لحذفها.");
 
@@ -229,11 +217,6 @@ namespace SafeTrace.Application.Services.UserProfileServices
         public async Task<ApiResponse<bool>> UpdatePhoneNumberAsync(string userId, ChangePhoneNumberDTO dto)
         {
             var user = await GetUser(userId);
-            //if (user == null)
-            //{
-            //    throw new NotFoundException("المستخدم غير موجود");
-            //}
-
             var newPhoneNumber = _mapper.Map(dto, user);
             var result = await _userManager.UpdateAsync(newPhoneNumber);
             if (!result.Succeeded)
