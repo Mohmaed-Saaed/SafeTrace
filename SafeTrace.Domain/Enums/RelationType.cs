2dc71557cbfbcf817b945f53ba1e3e4e40742dda
Mohamed-Gamal-Elemam <167555048+Mohamed-Gamal-Elemam@users.noreply.github.com>
2026-09-06 22:39:50 +0300

    fix

--- SafeTrace.Domain/Enums/RelationType.cs
+++ SafeTrace.Domain/Enums/RelationType.cs
@@ -6,6 +6,17 @@
         Mother,
         Brother,
         Sister,
+        Son,
+        Daughter,
+        Husband,
+        Wife,
+        Grandfather,
+        Grandmother,
+        Uncle,
+        Aunt,
+        Cousin,
+        Nephew,
+        Niece,
         Friend,
         Other
     }
